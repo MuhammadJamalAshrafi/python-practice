f62cac87f777eb889addc6745d6b926754012820
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2024-04-17 17:56:13 +0500

    Second Example in Numbers Learning

--- numbers_learning.py
+++ numbers_learning.py
@@ -9,4 +9,15 @@ football_field_area = football_field_length * football_field_width
 
 # Print out the total area of the football field
 # The f string formatting is used to insert the value of the football_field_area variable into the string
-print(f"Total area of football field is: {football_field_area} square meters")
+# print(f"Total area of football field is: {football_field_area} square meters")
+
+# 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar
+#    and you gave shopkeeper 20 dollar. Calculate the change using Python.
+
+num_packets = 9
+cost_per_packet = 1.49
+total_cost = num_packets * cost_per_packet
+money_paid = 20
+change = money_paid - total_cost
+
+print(f"The shopkeeper will give you back ${change:.2f}.")
