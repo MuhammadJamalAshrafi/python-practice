c57f5333e5d3ecd1122a4084ee9bbfa3782c0392
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2024-04-17 17:19:23 +0500

    Update

---
+++ numbers_learning.py
@@ -0,0 +1,12 @@
+#1.
+# Define the length and width of the football field
+football_field_length = 92  # in meters
+football_field_width = 48.8  # in meters
+
+# Calculate the area of the football field
+# The area of a rectangle is calculated by multiplying its length by its width
+football_field_area = football_field_length * football_field_width
+
+# Print out the total area of the football field
+# The f string formatting is used to insert the value of the football_field_area variable into the string
+print(f"Total area of football field is: {football_field_area} square meters")
---
+++ variables_learning.py
@@ -0,0 +1,10 @@
+birth_year=1998
+current_year=2024
+age=current_year-birth_year
+print(age)
+
+
+first_name = "Muhammad"
+middle_name = "Jamal"
+last_name = "Ashrafi"
+print("My full name is: " + first_name + " " + middle_name + " " + last_name)
